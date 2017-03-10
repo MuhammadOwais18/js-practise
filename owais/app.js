336145b2e79181b89410b752e83f404674d6a681
MuhammadOwais18 <mhanzala9999@gmail.com>
2017-03-10 12:31:15 +0500

    Add files via upload
    practice

---
+++ owais/app.js
@@ -0,0 +1,47 @@
+// var name="My name is owais.";
+// var age=19;
+// var name = name +age;
+// alert(name);
+// // document.write(name)
+// var num = 10;
+// num = num + num++ + num + num;
+// document.write(num);
+// var num =(2*3);
+// console.log(num);
+// var owais=5;
+var owais=prompt("Table");
+
+document.write("<br/>" +owais+ " x 1 = "+owais*1);
+document.write("<br/>" +owais+ " x 2 = "+owais*2);
+document.write("<br/>" +owais+ " x 3 = "+owais*3);
+document.write("<br/>" +owais+ " x 4 = "+owais*4);
+document.write("<br/>" +owais+ " x 5 = "+owais*5);
+document.write("<br/>" +owais+ " x 6 = "+owais*6);
+document.write("<br/>" +owais+ " x 7 = "+owais*7);
+document.write("<br/>" +owais+ " x 8 = "+owais*8);
+document.write("<br/>" +owais+ " x 9 = "+owais*9);
+document.write("<br/>" +owais+ " x 10 = "+owais*10);
+
+
+// console.error("owais");
+// console.warn("owais");
+// console.log("owais");
+// var name=owais;
+// alert("My name is" + name);
+// var name =prompt("your home?", "At Gulshan");
+// console.log(name);
+
+
+
+
+// var name="owais";
+// var age=55;
+
+// document.write(name + age);
+var name =prompt("Your Home?","At Gulshan");
+
+console.log("name");
+var name="20";
+var age=10;
+console.log(typeof name);
+console.log(typeof age);
